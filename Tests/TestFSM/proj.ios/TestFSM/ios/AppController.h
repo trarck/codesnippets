63431e7841164c8dac6c551d8548154b006a0377
duan.houhai <duan.houhai@denachina.com>
2012-09-12 19:09:20 +0800

    test fsm

---
+++ Tests/TestFSM/proj.ios/TestFSM/ios/AppController.h
@@ -0,0 +1,17 @@
+//
+//  TestFSMAppController.h
+//  TestFSM
+//
+//  Created by duanhouhai on 12-9-12.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@end
+
